37108f935dd688771891f5211e7cc07dbb4d08e1
Knut Zoch <knut.zoch@gmail.com>
2016-09-23 18:28:54 +0200

    Included ttbar comparison to the likelihood class

--- studies/likelihood.cc
+++ studies/likelihood.cc
@@ -17,6 +17,12 @@ void Likelihood::execute() {
   plotting::HistHolderContainer hist_container;
   hist_container.pullHistograms(file_container_, "h_likelihood");
 
+  HistHolderContainer hist_container_ttdil;
+  hist_container_ttdil.emplace(hist_container_ttdil.end(), new HistHolder{*hist_container.at(1)});
+  hist_container_ttdil.emplace(hist_container_ttdil.end(), new HistHolder{*hist_container.at(2)});
+
+  hist_container.erase(hist_container.end() - 2, hist_container.end() - 1);
+
   hist_container.at(0)->setDrawOptions("P E1");
   hist_container.at(0)->setLegendTitle("diboson");
   hist_container.at(0)->getHist()->SetLineColor(1);
@@ -46,7 +52,38 @@ void Likelihood::execute() {
   ratio_plotter.switchToMainPad();
   ratio_plotter.plotAtlasLabel();
   ratio_plotter.plotLegend();
-  ratio_plotter.saveToFile("likelihood");
+  ratio_plotter.saveToFile("separation_diboson");
+
+  // =================================================================
+
+  ratio_plotter.getAtlasLabel()->setAdditionalInfo("");
+  ratio_plotter.initCanvas();
+  ratio_plotter.initLegend();
+
+  hist_container_ttdil.at(0)->setDrawOptions("P E1");
+  hist_container_ttdil.at(0)->setLegendTitle("t#bar{t} + fake");
+  hist_container_ttdil.at(0)->getHist()->SetLineColor(1);
+  hist_container_ttdil.at(0)->getHist()->SetMarkerColor(1);
+  hist_container_ttdil.at(1)->setDrawOptions("P E1 SAME");
+  hist_container_ttdil.at(1)->setLegendTitle("t#bar{t}Z");
+  hist_container_ttdil.at(1)->getHist()->SetLineColor(2);
+  hist_container_ttdil.at(1)->getHist()->SetMarkerColor(2);
+
+  auto ratio_container_ttdil = hist_container_ttdil;
+  ratio_container_ttdil.divideHistograms(*hist_container_ttdil.at(0));
+
+  ratio_plotter.adjustLabels(&hist_container_ttdil, &ratio_container_ttdil);
+  ratio_plotter.addToLegend(hist_container_ttdil);
+
+  ratio_plotter.switchToHistPad();
+  hist_container_ttdil.setOptimalMax();
+  hist_container_ttdil.draw();
+  ratio_plotter.switchToRatioPad();
+  ratio_plotter.drawRatio(&ratio_container_ttdil);
+  ratio_plotter.switchToMainPad();
+  ratio_plotter.plotAtlasLabel();
+  ratio_plotter.plotLegend();
+  ratio_plotter.saveToFile("separation_ttdil");
 }
 }  // namespace studies
 }  // namespace plotting
